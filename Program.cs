081b433f0ff620cc3ab6dbb342f2937b1b28db47
Ricardo David Vaca Flores <ricardo.vaca@udla.edu.ec>
2023-11-27 11:20:09 -0500

    Azure Conectador

--- Program.cs
+++ Program.cs
@@ -15,11 +15,12 @@ builder.Services.AddDbContext<ApplicationDBContext>(option =>
 
 var app = builder.Build();
 
+app.UseSwagger();
+app.UseSwaggerUI();
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
-    app.UseSwagger();
-    app.UseSwaggerUI();
+    
 }
 
 app.UseAuthorization();
